771cebe1f43da555c4b78f42946fa23c08c4b4b9
JeonCM <wsd3576@gmail.com>
2025-05-08 16:06:23 +0900

    플레이어 정보 추가 및 점프로직과 슬라이딩, 충돌 조건 추가
    -아직 데미지 관련해서는 추가하지 않음
    -어째서인지 슬라이딩하면 빨라짐 수정예정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,45 +6,57 @@ public class Player : MonoBehaviour
 {
     Animator animator;
     Rigidbody2D _rigidbody;
+    CircleCollider2D _circleCollider;
 
-    public float flapForce = 6f;
-    public float forwardSpeed = 3f;
-    public bool isDead = false;
+    [Header("Player State")] //일단 다른데에서 쓸 수도 있으니 public으로 남겨둡니다.
+
+    public int HP = 100;
+    public float Speed = 3f;
+    public float JumpForce = 6f;
+
+    [SerializeField] private bool godMode = false;
+
     float deathCooldown = 0f;
+    float invincibleCooldown = 0f;
+    Vector2 originalColliderOffset = Vector2.zero;
+    float originalColliderSize = 0f;
 
-    bool isFlap = false;
-
-    public bool godMode = false;
+    bool isJump = false;
+    bool canJump = true;
+    bool isSlide = false;
+    bool isDead = false;
 
     GameManager gameManager;
 
-    // Start is called before the first frame update
     void Start()
     {
         gameManager = GameManager.Instance;
 
         animator = GetComponentInChildren<Animator>();
         _rigidbody = GetComponent<Rigidbody2D>();
+        _circleCollider = GetComponent<CircleCollider2D>();
 
         if (animator == null)
-            Debug.LogError("아 오류났어0 애니메이터 ");
+            Debug.LogError($"{gameObject.name}의 애니메이터를 찾을 수 없습니다. ");
 
         if (_rigidbody == null)
-            Debug.LogError("리지드바디 오류라고요");
+            Debug.LogError($"{gameObject.name}의 리지드바디를 찾을 수 없습니다.");
+
+        if (_circleCollider != null)
+        {
+            originalColliderOffset = _circleCollider.offset;
+            originalColliderSize = _circleCollider.radius;
+        }
+        else Debug.LogError($"{gameObject.name}의 서클콜라이더를 찾을 수 없습니다.");
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (isDead)
         {
             if (deathCooldown <= 0)
             {
-                // 게임 재시작
-                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-                {
-                    gameManager.RestartGame();
-                }
+                //게임오버 로직
             }
             else
             {
@@ -53,12 +65,11 @@ public class Player : MonoBehaviour
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-            {
+            invincibleCooldown -= Time.deltaTime;
+            if (canJump && Input.GetKeyDown(KeyCode.Space))
+                isJump = true;
 
-                isFlap = true;
-
-            }
+            isSlide = Input.GetKey(KeyCode.LeftShift);
         }
     }
 
@@ -69,16 +80,27 @@ public class Player : MonoBehaviour
         if (isDead) return;
 
         Vector3 velocity = _rigidbody.velocity;
-        velocity.x = forwardSpeed;
+        velocity.x = Speed;
 
-        if (isFlap)
+        if (isJump)
         {
-            velocity.y += flapForce;
-            isFlap = false;
-
+            velocity.y += JumpForce;
+            isJump = false;
+            canJump = false;
         }
-        _rigidbody.velocity = velocity;
 
+        if (isSlide)
+        {
+            _circleCollider.offset = new Vector2(0f, -0.3f);
+            _circleCollider.radius = 0.25f;
+        }
+        else if (!isSlide)
+        {
+            _circleCollider.offset = originalColliderOffset;
+            _circleCollider.radius = originalColliderSize;
+        }
+
+        _rigidbody.velocity = velocity;
 
         float angle = Mathf.Clamp((_rigidbody.velocity.y * 10f), -90, 90);
         transform.rotation = Quaternion.Euler(0, 0, angle);
@@ -86,19 +108,33 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
-        if (godMode) return;
+        if (collision.gameObject.tag == "BackGround") //바닥과 닿아야만 다시 점프 가능
+        {
+            canJump = true;
+            Debug.Log("점프 가능!");
+        }
 
-        if (isDead) return;
+        if (godMode || isDead) return; //갓모드거나 죽었다면 계산 안함
 
-        isDead = true;
-        deathCooldown = 1f;
+        if (collision.gameObject.tag == "Obstacle" && invincibleCooldown > 0) return; //장애물이랑 충돌했는데 무적시간이면 계산안함
 
-        animator.SetInteger("IsDie",1);
+        int value = 0;
+        Obstacle obstacle = collision.gameObject.GetComponent<Obstacle>();
 
-        gameManager.GameOver();
+        if (obstacle != null)
+        {
+            //장애물의 값을 받아 플레이어 체력에 합산하는 부분
+            value = 000;
+            HP += value;
+        }
 
-
+        /* 아이템의 값을 받아 플레이어 체력에 합산하는 부분(아이템 추가 이후 작동하도록 주석화)
+        Item item = collision.gameObject.GetComponent<Item>();
+        if(item != null)
+        {
+            value = 000;
+            HP += value;
+        }
+        */
     }
-
-
 }
