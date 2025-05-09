277ef396fcbd2fcbc44caa0edff4aa5743a8472e
Kyllox <kyllox4804@gmail.com>
2025-05-09 15:40:07 +0900

    feat: 기초적인 UI 구현

---
+++ Assets/Scripts/BaseUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIState
+{
+    Home,
+    InGame,
+    GameOver
+}
+
+public abstract class BaseUI : MonoBehaviour
+{
+    public virtual void Init()
+    {
+
+    }
+
+    protected abstract UIState GetUIState();
+
+    public void SetActive(UIState state)
+    {
+        gameObject.SetActive(GetUIState() == state);
+    }
+}
---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : BaseUI
+{
+    public override void Init()
+    {
+        Time.timeScale = 0f;
+        base.Init();
+    }
+
+    protected override UIState GetUIState()
+    {
+        return UIState.GameOver;
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            GameManager.Instance.StartGame();
+        }
+    }
+}
