10ced3695f83519713f9bf228dfec9d6d2282715
TaeYoungTOF <113530625+TaeYoungTOF@users.noreply.github.com>
2025-05-09 19:51:19 +0900

    refactor: intergrate ItemObject and ObstacleObject into Object
    - 자식클래스 ItemObject와 ObstacleObject를 부모클래스 Object로 통합

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -1,34 +1,86 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public enum ObjectType
 {
-    Obstacle,
-    Item
+    Coin,
+    SpeedUp,
+    SpeedDown,
+    Heal,
+
+    NormalObstacle,
+    Arrow,
+    EndPoint
 }
 
-public abstract class Object : MonoBehaviour
+public class Object : MonoBehaviour
 {
-    public abstract void OnTriggerEnter2D(Collider2D collision);    
+    [SerializeField] private ObjectType _objectType;
 
-    public virtual void DestroyTile(Collider2D collision)
+    public ObjectType ObjectType { get => _objectType; }
+    private string _objectName;
+    public string ObjectName { get => _objectName; }
+
+    private void Start()
     {
-        // 임시 코드
-        if (collision.gameObject.name == "Opstacle") return;
-        //
+        _objectName = gameObject.name;
+    }
 
-        if (collision.CompareTag("Player"))
-        {
-            Debug.Log("Destroy 실행 준비");
-            Tilemap tilemap = gameObject.GetComponent<Tilemap>();
-            if (tilemap != null)
-            {
-                Vector3 hitPoint = collision.ClosestPoint(transform.position);
-                Vector3Int cellPosition = tilemap.WorldToCell(hitPoint);
-                if (tilemap.HasTile(cellPosition))
-                    tilemap.SetTile(cellPosition, null);
-            }
-            Debug.Log("Destroy 실행 완료");
-        }
+    #region 충돌 관련 메서드
+    public void Coin()
+    {
+        Debug.Log("Coin");
+        GameManager.Instance.UpdateScore(1);
+    }
+
+    public void SpeedUp()
+    {
+        Debug.Log("SpeedUp");
+        GameManager.Instance.ChangePlayerSpeed(10f);
+    }
+
+    public void SpeedDown()
+    {
+        Debug.Log("SpeedDown");
+        GameManager.Instance.ChangePlayerSpeed(-10f);
+    }
+
+    public void Heal()
+    {
+        const float healAmount = 10.0f;
+
+        Debug.Log("Heal");
+        GameManager.Instance.ChangePlayerHP(healAmount);
+    }
+
+    public void NormalObstacle()
+    {
+        const float _normalDamage = 10.0f;
+
+        Debug.Log("NormalObstacle");
+        GameManager.Instance.ChangePlayerHP(-_normalDamage);
+    }
+
+    public void Arrow()
+    {
+        const float _ArrowDamage = 10.0f;
+
+        Debug.Log("Arrow");
+        GameManager.Instance.ChangePlayerHP(-_ArrowDamage);
+    }
+
+    public void EndPoint()
+    {
+        Debug.Log("EndPoint");
+        GameManager.Instance.GameOver();
+    }
+
+    #endregion
+
+    // SpawnManager로 이관?
+    private void MoveArrow(GameObject gameObject)
+    {
+        const float arrowSpeed = 5.0f;
+
+        gameObject.transform.position += arrowSpeed * Time.deltaTime * Vector3.left;
     }
 }
