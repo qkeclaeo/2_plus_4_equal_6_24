f0e7417245a0cee0d6f29b8a70f0975e9730d505
kyvv08 <ky4995@naver.com>
2025-05-12 20:30:33 +0900

    SpawnManager 변경
    x좌표의 고정값으로 맵을 배치하도록 변경
    spawn매니저 내에서 프리팹 수 * 2 만큼 생성 후 재배치로 사용

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,46 +1,115 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class SpawnManager : MonoBehaviour
 {
-    static SpawnManager spawnManager;
-    public static SpawnManager Instance { get { return spawnManager; } }
+    [SerializeField] private float offsetX;
+    float nextPosX = 0f;
+    public static SpawnManager Instance { get; private set; }
 
-    [SerializeField] private int numBgCount = 5;
-    [SerializeField] private Transform playerTransform;
+    public GameObject[] mapPrefabs;
 
+    public Transform spawnPoint;
+//    private Transform lastMapPos = null;
+
+    Queue<GameObject> mapsQueue;
+    //List<GameObject> mapsPool;
+    List<int> prefabIndex;
+
+    int mapCount = 0;
+
+    bool isInfinite = false;
+    
     private void Awake()
     {
-        spawnManager = this;
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        mapsQueue = new Queue<GameObject>();
+    }
+    void Start()
+    {
+        prefabIndex = new List<int>();
+        for (int i = 0; i < mapPrefabs.Length; ++i) {
+            prefabIndex.Add(i);
+        }
+        for (int i = 0; i < 2; ++i)
+        {
+            ShuffleIndex();
+            for (int j = 0; j < mapPrefabs.Length; ++j)
+            {
+                GameObject obj = Instantiate(mapPrefabs[prefabIndex[j]]);
+                obj.transform.position = new Vector3(nextPosX, 0, 0);
+                nextPosX += offsetX;
+                obj.SetActive(false);
+                mapsQueue.Enqueue(obj);
+            }
+        }
+    }
+    public void Init()
+    {
+        nextPosX = 0;
+        foreach(var obj in mapsQueue)
+        {
+            obj.SetActive(true);
+            obj.transform.position = new Vector3(nextPosX,0,0);
+            nextPosX += offsetX;
+        }
+    }
+    public void GameOver()
+    {
+        foreach (var obj in mapsQueue)
+        {
+            obj.SetActive(false);
+        }
+    }
+    public void SpawnMap()
+    {
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void SetInfiniteMode(bool istrue)
     {
-        // Background 태그가 붙어있는 오브젝트 순환
-        if (collision.CompareTag("BackGround"))
+        isInfinite = istrue;
+    }
+
+    void ShuffleIndex()
+    {
+        for (int i = prefabIndex.Count - 1; i > 0; --i)
         {
-            Debug.Log($"Triggerd : {collision.name}");
-
-            float widthOfBgObject = ((BoxCollider2D)collision).size.x;
-            Vector3 pos = collision.transform.position;
-
-            pos.x += widthOfBgObject * numBgCount;
-            collision.transform.position = pos;
-            return;
+            int j = Random.Range(0, i + 1);
+            int temp = prefabIndex[j];
+            prefabIndex[j] = prefabIndex[i];
+            prefabIndex[i] = temp;
         }
-
-        /**
-        // Object 태그가 붙어있는 오브젝트(Item/Obstacle) 파괴
-        Debug.Log($"Object Destroy : {collision.name}");
-
-        Tilemap tilemap = collision.gameObject.GetComponent<Tilemap>();
-        if (tilemap != null)
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(isInfinite && collision.CompareTag("MapPrefab"))
         {
-            Vector3 hitPoint = collision.ClosestPoint(transform.position);
-            Vector3Int cellPosition = tilemap.WorldToCell(hitPoint);
-            if (tilemap.HasTile(cellPosition))
-                tilemap.SetTile(cellPosition, null);
+            ++mapCount;
+            if (mapCount >= mapPrefabs.Length)
+            {
+                List<GameObject> temp = new List<GameObject>();
+                for (int i = 0; i < mapPrefabs.Length; ++i)
+                {
+                    temp.Add(mapsQueue.Dequeue());
+                }
+                ShuffleIndex();
+
+                for (int i = 0; i < temp.Count; ++i)
+                {
+                    //temp[prefabIndex[i]].Init();        //초기화 함수
+                    mapsQueue.Enqueue(temp[prefabIndex[i]]);
+                }
+                mapCount = 0;
+            }
         }
-        */
     }
 }
