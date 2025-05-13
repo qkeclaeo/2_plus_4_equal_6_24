246829758c98c9132f321abc8bff865b413e9318
JeonCM <wsd3576@gmail.com>
2025-05-13 17:22:31 +0900

    프리팹 이름 재수정 및 모드 선택 UI 추가
    -정리
    --스크립트 폴더에 UI폴더 추가
    --ModeSelectUI.cs와 PlayerSelectUI.cs 스크립트 추가
    --프리팹 폴더에 UI폴더 추가
    --ModeSelectUI와 PlayerSelectUI프리팹 추가

--- Assets/Scripts/UI/PlayerSelectUI.cs
+++ Assets/Scripts/UI/PlayerSelectUI.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SelectPlayer : MonoBehaviour
+public class PlayerSelectUI : MonoBehaviour
 {
     GameObject[] playerObjects;
     Player[] players;
