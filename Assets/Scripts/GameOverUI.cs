934683d43935975af478cc5e1b0959229014bdff
kyvv08 <ky4995@naver.com>
2025-05-14 15:58:44 +0900

    Home버튼 추가

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverUI : BaseUI
@@ -9,13 +10,20 @@ public class GameOverUI : BaseUI
     [SerializeField] private TextMeshProUGUI _curScoreText;
     [SerializeField] private TextMeshProUGUI _maxScoreText;
     [SerializeField] private Button _restartBtn;
+    [SerializeField] private Button _homeBtn;
 
     public override void Init()
     {
         _restartBtn.onClick.RemoveAllListeners();
+        _homeBtn.onClick.RemoveAllListeners();
+
         _restartBtn.onClick.AddListener(delegate
         {
             GameManager.Instance.Restart();
+        });
+        _homeBtn.onClick.AddListener(delegate
+        {
+            SceneManager.LoadScene("SampleScene");
         });
     }
 
